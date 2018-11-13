f3e2bed4871cf776e942cd65815652515c000bae
Lulu Chang <luluchang@Lulude-MacBook-Air.local>
2018-11-13 15:30:48 +0100

    vue

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,40 +1,40 @@
 window.onload = function () {
 	
-	class MenuItem {
-	  constructor(name, kCal, if_gluten, if_lactose) {
-		this.Name = name;
-		this.KCal =kCal;
-		this.If_gluten =if_gluten;
-		this.If_lactose =if_lactose;
-	  }
+	// class MenuItem {
+	//   constructor(name, kCal, if_gluten, if_lactose) {
+	// 	this.Name = name;
+	// 	this.KCal =kCal;
+	// 	this.If_gluten =if_gluten;
+	// 	this.If_lactose =if_lactose;
+	//   }
 
-	  get name() {
-		return this.Name;
-	  }
-	  get kCal() {
-		return this.KCal;
-	  }
-	  get if_gluten(){
-		return this.If_gluten;
-	  }
-	  get if_lactose(){
-		return this.If_lactose;
-	  }  
-	}
+	//   get name() {
+	// 	return this.Name;
+	//   }
+	//   get kCal() {
+	// 	return this.KCal;
+	//   }
+	//   get if_gluten(){
+	// 	return this.If_gluten;
+	//   }
+	//   get if_lactose(){
+	// 	return this.If_lactose;
+	//   }  
+	// }
 
-	function create_burger(){
-		var che = new MenuItem("cheese burger",123,true,false);
-		var pork = new MenuItem("pork burger",123,true,true);
-		var chi = new MenuItem("chicken burger",123,true,false);
-		var veg = new MenuItem("vegan burger",123,false,false);
-		var mb = new MenuItem("meatball burger",123,false,true);
-		var burgers = [che,pork,chi,veg,mb];
-		return burgers;
-	}
+	// function create_burger(){
+	// 	var beef = new MenuItem("beef burger",123,true,false);
+	// 	var cheese = new MenuItem("cheese burger",123,true,true);
+	// 	var chicken = new MenuItem("chicken burger",123,true,false);
+	// 	var veggie = new MenuItem("veggie burger",123,false,false);
+	// 	var bacon = new MenuItem("bacon burger",123,false,true);
+	// 	var burgers = [beef,cheese,chicken,veggie,bacon];
+	// 	return burgers;
+	// }
 
 
 	// var food = create_burger();
-	// var burgers_src = ['che.jpg','pork.jpg','chi.jpg','veg.jpg','mb.jpg'];
+	// var burgers_src = ['che.jpg','pork.jpg','chi.jpg','veg.jpg','mb.jpg']; 
 
 
 	var burgers_props = [];
@@ -73,6 +73,9 @@ window.onload = function () {
 		el: '#orders',
 		methods: {
 			markDone: function() {
+
+				document.getElementById('customer_details').innerHTML="";
+				document.getElementById('order_summary').innerHTML="";
 				
 				// Read gender radio
 				var gender;
@@ -99,12 +102,20 @@ window.onload = function () {
 				var submit_form = {
 					"fullname":document.getElementById('fullname').value,
 					"email":document.getElementById('email').value,
-					"street":document.getElementById('street').value,
-					"house":document.getElementById('house').value,
+					// "street":document.getElementById('street').value,
+					// "house":document.getElementById('house').value,
 					"payment":document.getElementById('payment').value,
 					"gender":gender,
-					"burgers":burgers
+					// "burgers":burgers
 				};
+
+				if(submit_form.fullname==''||submit_form.email==''||submit_form.payment==''||submit_form.gender==''||burgers.length==0){
+					document.getElementById('success').innerHTML="Form not completed!!";
+					return;
+				}
+				else{
+					document.getElementById('success').innerHTML="Success!!";
+				}
 				
 				for(var i in submit_form){
 					var node = document.createElement("div");
@@ -113,6 +124,28 @@ window.onload = function () {
 					document.getElementById('be_submitted').appendChild(node);
 				}
 
+				for(var i=0; i<burgers.length; i++){
+					var node = document.createElement("li");
+					var textnode = document.createTextNode(burgers[i]);
+					node.appendChild(textnode);
+					document.getElementById('order_summary').appendChild(node);
+				}
+
+				var map = document.getElementById('map');
+				var style = window.getComputedStyle(map);
+				var left_x = style.getPropertyValue('left');
+				var top_y = style.getPropertyValue('top');
+				socket.emit("addOrder", {
+										
+										details: { x: parseFloat(left_x),
+												   y: parseFloat(top_y) },
+										orderItems: burgers,
+										fullname: submit_form.fullname,
+										gender: submit_form.gender,
+										email: submit_form.email,
+										payment: submit_form.payment
+										
+									  });		
 			}
 		}
 	});
